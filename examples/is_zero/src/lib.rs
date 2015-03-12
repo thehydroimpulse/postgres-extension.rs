35a16d408d81be49541e63b6db53ac02ca37fb4e
Daniel Fagnan <dnfagnan@gmail.com>
2015-03-12 15:00:06 -0600

    Fix build. Closes #4

--- examples/is_zero/src/lib.rs
+++ examples/is_zero/src/lib.rs
@@ -1,15 +1,16 @@
-#![feature(phase)]
+#![feature(libc, custom_attribute, plugin)]
+#![plugin(postgres_extension_macros)]
 
 extern crate postgres_extension;
 
-#[phase(plugin)]
+#[macro_use]
 extern crate postgres_extension_macros;
 
 extern crate libc;
 
 use libc::{c_int};
 
-pg_module!(version: 90500)
+pg_module!(version: 90500);
 
 #[pg_export]
 pub fn is_zero(a: c_int) -> c_int {
